ebea7d35347e2a71a7e43293effe524877d85228
daj21115 <dwaynedaj@gmail.com>
2026-03-29 20:28:03 -0400

    Add automation for updating ossec.conf with Sysmon logs
    Automates the addition of a localfile entry for Sysmon logs in ossec.conf using XML parsing.

--- scripts/powershell/update-ossec-config.ps1
+++ scripts/powershell/update-ossec-config.ps1
@@ -1,1 +1,14 @@
 update-ossec-config.ps1
+
+<#
+Name: update-ossec-config.ps1
+Description: Updates ossec.conf by adding a localfile entry for Sysmon logs using XML parsing.
+Use Case: Automates configuration changes and bypasses manual permission issues.
+#>
+
+# Converting the config to an XML object to bypass permission errors
+$xml = [xml](Get-Content "C:\Program Files (x86)\ossec-agent\ossec.conf")
+# Appending the Sysmon channel
+$newLocalFile = $xml.CreateElement("localfile")
+# ... (Adding location and log_format tags)
+$xml.Save("C:\Program Files (x86)\ossec-agent\ossec.conf")
